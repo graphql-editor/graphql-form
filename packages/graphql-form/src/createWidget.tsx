e4861117a3c228607bda92d2f8f12ad7cf6fafa2
aexol <aexol@aexol.com>
2022-06-13 13:59:14 +0200

    :sparkles: Added display name

--- packages/graphql-form/src/createWidget.tsx
+++ packages/graphql-form/src/createWidget.tsx
@@ -7,14 +7,17 @@ export function createWidget<Props>({
     Settings,
     Description,
     requirements,
+    displayName,
 }: {
     name: string;
+    displayName?: string;
     Component: React.FC<PassedFormProps<Props>>;
     Settings?: React.FC<CastToWidgetSettingsPassedForm<Props>>;
     Description?: React.FC;
     requirements: (props: PassedFormProps) => boolean;
 }) {
     return {
+        displayName,
         Component,
         Settings,
         Description,
