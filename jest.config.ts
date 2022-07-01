1303631adfcca1fa7c587e5a35139d6f7069ba72
JedrekDabrowski <jedrzejdabrowski.jd@gmail.com>
2022-07-01 10:52:12 +0200

    🔧  add config jest and eslint

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'ts-jest',
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    moduleNameMapper: {
+        '@/(.*)': ['<rootDir>/src/$1'],
+    },
+    testMatch: ['**/*.spec.(ts|tsx)'],
+    watchPathIgnorePatterns: ['node_modules'],
+    watchman: false,
+};
