41a5735d0611d4e0ff17ad74bd69234c56c19fe8
aexol <aexol@aexol.com>
2023-01-23 16:04:30 +0100

    :arrow_up: upgraded js tree

--- packages/graphql-form/src/__tests__/consts.ts
+++ packages/graphql-form/src/__tests__/consts.ts
@@ -1,33 +1,33 @@
 import { FormObject } from '@/models';
-import { Options, ParserField, TypeDefinition } from 'graphql-js-tree';
+import { createParserField, Options, ParserField, TypeDefinition } from 'graphql-js-tree';
 
 // Global Arrange
-export const typeNode: ParserField = {
+export const typeNode: ParserField = createParserField({
     name: 'Type',
     args: [],
     data: { type: TypeDefinition.ObjectTypeDefinition },
     directives: [],
     interfaces: [],
     type: { fieldType: { name: 'type', type: Options.name } },
-};
+});
 
-export const inputNode: ParserField = {
+export const inputNode: ParserField = createParserField({
     name: 'Input',
     args: [],
     data: { type: TypeDefinition.InputObjectTypeDefinition },
     directives: [],
     interfaces: [],
     type: { fieldType: { name: 'type', type: Options.name } },
-};
+});
 
-export const nodeWithArgs: ParserField = {
+export const nodeWithArgs: ParserField = createParserField({
     name: 'Root',
     args: [typeNode, inputNode],
     data: { type: TypeDefinition.ObjectTypeDefinition },
     directives: [],
     interfaces: [],
     type: { fieldType: { name: 'type', type: Options.name } },
-};
+});
 
 export const singleNode: ParserField[] = [typeNode];
 export const nodes: ParserField[] = [typeNode, inputNode, nodeWithArgs];
--- packages/zeus-form/src/__tests__/consts.ts
+++ packages/zeus-form/src/__tests__/consts.ts
@@ -1,40 +1,44 @@
 import { FormObject } from '@/models';
-import { Options, ParserField, TypeDefinition } from 'graphql-js-tree';
+import { createParserField, Options, ParserField, TypeDefinition } from 'graphql-js-tree';
 
 // Global Arrange
-export const typeNode: ParserField = {
+export const typeNode: ParserField = createParserField({
     name: 'Type',
     args: [],
     data: { type: TypeDefinition.ObjectTypeDefinition },
     directives: [],
     interfaces: [],
     type: { fieldType: { name: 'type', type: Options.name } },
-};
+});
 
-export const inputNode: ParserField = {
+export const inputNode: ParserField = createParserField({
     name: 'Input',
     args: [],
     data: { type: TypeDefinition.InputObjectTypeDefinition },
     directives: [],
     interfaces: [],
     type: { fieldType: { name: 'type', type: Options.name } },
-};
+});
 
-export const nodeWithArgs: ParserField = {
+export const nodeWithArgs: ParserField = createParserField({
     name: 'Root',
     args: [typeNode, inputNode],
     data: { type: TypeDefinition.ObjectTypeDefinition },
     directives: [],
     interfaces: [],
     type: { fieldType: { name: 'type', type: Options.name } },
-};
+});
 
 export const singleNode: ParserField[] = [typeNode];
 export const nodes: ParserField[] = [typeNode, inputNode, nodeWithArgs];
 
-export const fields: Record<string, FormObject> = { test: { node: typeNode, value: 5 } };
-export const extenedFields: Record<string, FormObject> = { testInput: { node: inputNode, value: fields } };
-export const fieldsNodeWithArgs: Record<string, FormObject> = { testInput: { node: nodeWithArgs, value: fields } };
+export const fields: Record<string, FormObject> = { test: { __form__node: typeNode, __form__value: 5 } };
+export const extenedFields: Record<string, FormObject> = {
+    testInput: { __form__node: inputNode, __form__value: fields },
+};
+export const fieldsNodeWithArgs: Record<string, FormObject> = {
+    testInput: { __form__node: nodeWithArgs, __form__value: fields },
+};
 export const fieldsNodeWithArgsWithInput: Record<string, FormObject> = {
-    testInput: { node: nodeWithArgs, value: extenedFields },
+    testInput: { __form__node: nodeWithArgs, __form__value: extenedFields },
 };
